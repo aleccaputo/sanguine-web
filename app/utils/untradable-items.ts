0fad5337113812f310aa37c1e2d56dc8d4a52959
Alec Caputo <aleccaputo7@gmail.com>
2025-10-10 16:53:42 -0400

    fix untradables

--- app/utils/untradable-items.ts
+++ app/utils/untradable-items.ts
@@ -14,52 +14,26 @@ export const untradeableItems: Record<number, string> = {
   // Pet (generic pet entry - all pets are untradeable)
   0: 'pet',
 
-  // Avernic defender hilt becomes untradeable when combined
-  22477: 'avernic defender hilt',
-
-  // Teleport anchoring scroll (untradeable reward)
-  29455: 'teleport anchoring scroll',
-
-  // Broken/damaged items (typically untradeable until repaired)
-  28813: 'broken zombie axe',
-  30324: 'broken zombie helmet',
-  26376: 'torva full helm (damaged)',
-  26378: 'torva platebody (damaged)',
-  26380: 'torva platelegs (damaged)',
-
   // Metamorphic dust (untradeable CoX reward)
   22386: 'metamorphic dust',
 
   // Dust from CoX (untradeable)
   24670: 'twisted ancestral colour kit',
 
-  // Ornament kits (become untradeable when used)
-  20062: 'torture ornament kit',
-  22246: 'anguish ornament kit',
-  23348: 'tormented ornament kit',
   25744: 'sanguine ornament kit',
   12526: 'holy ornament kit',
 
   // Prayer scrolls (consumed on use, effectively untradeable)
-  21034: 'dexterous prayer scroll',
-  21079: 'arcane prayer scroll',
   30627: 'mystic vigour prayer scroll',
   30626: 'deadeye prayer scroll',
 
   // Tool/utility items that become untradeable
-  22994: 'bottomless compost bucket',
   25582: 'fish barrel',
   25580: 'tackle box',
   24482: 'seed box',
 
   // Quest/minigame related items
   24880: "amy's saw",
-
-  // Eternal gem (untradeable slayer reward)
-  21270: 'eternal gem',
-
-  // Amulet of eternal glory (untradeable version)
-  19707: 'amulet of eternal glory',
 
   // Expert mining gloves (untradeable reward)
   21392: 'expert mining gloves',
@@ -72,9 +46,6 @@ export const untradeableItems: Record<number, string> = {
 
   // Curved bone (untradeable prayer item)
   10977: 'curved bone',
-
-  // Brimstone/Hydra items that are untradeable versions
-  20724: 'imbued heart',
 
   29265: 'guild hunter top',
   29267: 'guild hunter legs',
